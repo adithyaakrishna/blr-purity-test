5379aad70010cf6d0a386dfa8467cbc1a89d7990
Adithya Krishna <aadithya794@gmail.com>
2025-01-03 04:35:13 +0530

    chore: change to blr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default function PurityTest() {
       <div className="max-w-3xl mx-auto">
        <div className="text-center mb-8">
         <h1 className="text-4xl font-bold tracking-tight text-orange-500 mb-4">
-          Bengaluru Purity Test
+          Blr Purity Test
         </h1>
         <p className="text-lg text-gray-600 mb-3">
           Namaskara! 🙏 <br /> The official checklist for techies surviving and thriving in India&apos;s Silicon Valley.
